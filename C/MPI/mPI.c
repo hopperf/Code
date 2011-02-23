98ecc554b1e0a23778d7f30373214da579331ec2
Franz Hopper <hopperf@lavabit.com>
2011-02-23 15:15:47 -0500

    removed mpiTiming.c and made mPI.c sense im putting so much time into the PI program with timing i might as well make the whole thing dedicated to PI. Yet to reimplement timing but its rather simple now that i know what the hell is going on. GPL and header are good for now will add some more in later mabey.

---
+++ C/MPI/mPI.c
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * FILE: mPI.c
+ * VERSION: 0.1
+ *
+ * DESCRIPTION: An example MPI program that calculates PI and times how 
+ * long the total procedure takes.
+ *
+ * OPTIONS: ---
+ * REQUIREMENTS: MPI
+ *
+ * AUTHOR: Franz Hopper
+ * EMAIL:  hopperf@lavabit.com
+ * DATE:   2/21/2011  
+ ******************************************************************************/
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[]){
+  int numProcs, rank;
+  double start_time, end_time, processing_time;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm commRail; // Init communicator
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcs); // Total number of processes
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Which process am I?
+  MPI_Comm_split( MPI_COMM_WORLD, rank == 0, 0, &commRail ); // Create communicator 
+
+  if(rank == 0){ // I am the master then
+    master(MPI_COMM_WORLD, commRail);
+  }
+  else{ // Crap, I'm a node then
+    node(MPI_COMM_WORLD, commRail);
+  }
+  MPI_Finalize();
+  return 0;
+}
+
+// Master 
+int master(mCommunicator, commToUse)
+MPI_Comm commToUse;
+{
+  int  i,j, size;
+  char buf[256];
+  
+  MPI_Status status;
+  MPI_Comm_size(mCommunicator, &size);
+  
+  //for (j=1; j<=2; j++) {
+    for (i=1; i<size; i++) {
+      MPI_Recv(buf, 256, MPI_CHAR, i, 0, mCommunicator, &status);
+      fputs(buf, stdout);
+    }
+  //}
+  printf("this is the end of exec\n");
+}
+  
+// Node
+int node(mCommunicator, commToUse)
+MPI_Comm commToUse;
+{
+    char buf[256];
+    int	 rank;
+    
+    MPI_Comm_rank(commToUse, &rank);
+    
+    sprintf(buf, "Hello from slave %d\n", rank);
+    MPI_Send(buf, strlen(buf) + 1, MPI_CHAR, 0, 0, mCommunicator);
+    
+   // sprintf(buf, "Goodbye from slave %d\n", rank);
+    //MPI_Send(buf, strlen(buf) + 1, MPI_CHAR, 0, 0, mCommunicator);
+    return 0;
+}
+
+/*******************************************************************************
+ * mPI - An example MPI program for calculating PI with timing.
+ * Copyright (C) 2011 Franz Hopper
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
